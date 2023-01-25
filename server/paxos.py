0abd618026640d96f53bfb97283fc257e7df83c0
TarasZhere <zhere97@gmail.com>
2023-01-25 10:45:51 -0500

    init

--- server/paxos.py
+++ server/paxos.py
@@ -1,7 +1,7 @@
 from random import randint 
 import sys
 from threadingReturn import ThreadReturn as Thread
-
+from rpc import RPCClient
 
 class Clause:
     def __init__(self, identifier=-1, value=None) -> None:
@@ -13,13 +13,32 @@ class Paxos(object):
     def __init__(self) -> None:
         self.proposer = Clause()
         self.acceptor = Clause()
+        self.instances = [
+            RPCClient(('localhost', 8000)),
+            RPCClient(('localhost', 8001)),
+            RPCClient(('localhost', 8002)),
+        ]
     
-    def test() -> None:
-        print('Test was a success')
+    def paxos(self, value):
+        currentIdentifier = self.proposer.identifier + randint(1,2)
+        threads = [
+            Thread(target=self.prepare, args=[instance, currentIdentifier]).start() for instance in self.instances
+        ]
+
+        responses = [
+            thread.join(3) for thread in threads
+        ]
+
+        print(responses)
+
         pass
 
-    def prepare(self) -> int:
-        return self.proposer.identifier + randint(1, 2)
+
+    def prepare(self, instance, identifier) -> int:
+        res = instance.promise(identifier)
+        return res
+        
+
 
     def promise(self, identifier) -> int:
         print(f'! Promise request of identifier: {identifier} recieved')
@@ -36,6 +55,3 @@ class Paxos(object):
                 f.write(f'{identifier}:{value}\n')
 
         return {'accept':True}
-
-    def test(self):
-        return 'success'
