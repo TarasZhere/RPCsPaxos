f426e9f07744ad1489a1e19b89e2ead96e4868ce
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2023-02-05 13:17:07 -0500

    ports set client

--- client.py
+++ client.py
@@ -1,10 +1,19 @@
 from server.rpc import RPCClient
 from threading import Thread
 from random import randint
+import sys
+
+try: HOST = sys.argv[1] # The server's hostname or IP address
+except: HOST = "10.128.0.2"  
 
 
-HOST = "10.128.0.2"  # The server's hostname or IP address
-PORT = 8080  # The port used by the server
+try: PORT = int(sys.argv[2]) # The port used by the server
+except: PORT = 8080 
+
+
+try: numReq = int(sys.argv[3])
+except: numReq = 1
+
 
 from time import sleep
 # print(server.isConnected())
@@ -14,6 +23,6 @@ def multy():
     server.connect()
     print(server.paxos(randint(0,10)))
 
-for _ in range(1):
+for _ in range(numReq):
     Thread(target=multy).start()
 
