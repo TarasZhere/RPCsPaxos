bcef864f1e47b0e63a3f425800960c6726676488
taras_zhere <35704223+TarasZhere@users.noreply.github.com>
2023-02-05 13:05:18 -0500

    docs

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ from threading import Thread
 from random import randint
 
 
-HOST = "10.128.0.0"  # The server's hostname or IP address
-PORT = 20  # The port used by the server
+HOST = "10.128.0.2"  # The server's hostname or IP address
+PORT = 8080  # The port used by the server
 
 from time import sleep
 # print(server.isConnected())
