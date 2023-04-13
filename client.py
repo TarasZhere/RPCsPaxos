597bbd43c8cd6504e069ea32614c78035b4659ea
TarasZhere <zhere97@gmail.com>
2023-04-13 15:08:38 -0400

    init

--- client.py
+++ client.py
@@ -1,44 +1,21 @@
 from server.rpc import RPCClient
 from threading import Thread
 from random import randint
-import click
 
-@click.command()
-@click.option('--host', default='0.0.0.0', help='Host IP addr')
-def setHost(host):
-    global HOST
-    HOST = host
-    pass
-
-
-@click.command()
-@click.option('--port', default=80, help='Port number')
-def setPort(port):
-    global PORT
-    PORT = port
-    pass
-
-@click.command()
-@click.option('--req_num', default=1, help='Number of concurrent requests you want to test Paxos with.')
-def set_req(req_num):
-    global REQ
-    REQ = req_num
-    pass
+HOST='35.188.190.168'
+PORT=80
+REQ=1
 
 # This functions creates concurrent definitions with a random sleep between requests to random servers
-def concurrentDefinition(id = 0):
+def concurrentDefinition():
     server = RPCClient((HOST, PORT))
     server.connect()
     response = server.consensus(randint(1,10))
     print(response)
 
 
-if __name__ == "__main__":
 
-    setHost()
-    set_req()
-    setPort()
+for _ in range(REQ):
 
-    for _ in range(REQ):
-        Thread(target=concurrentDefinition, args=[REQ]).start()
+    Thread(target=concurrentDefinition, args=[REQ]).start()
 
